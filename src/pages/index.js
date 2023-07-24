b01f95528e02af848040cc7a7d743007a5921ee7
Claudio Ventimiglia <claudioxxmilia@yahoo.it>
2023-07-24 16:08:44 +0200

    firstCommit/reset

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+import styles from "@/styles/Home.module.css";
+
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Cb-7 Final Project gruppo-B</title>
+        <meta name="description" content="Cb-7 Final Project gruppo-B" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main ></main>
+    </>
+  );
+}
