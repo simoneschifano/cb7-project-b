1cc88ed55b7584f20e7250c9bad9643307c69016
GabSalemi <salemi.gabriele94@gmail.com>
2023-07-26 15:57:45 +0200

    feat: add swiper

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,6 +8,12 @@ import Navbar from "@/components/Navbar";
 
 import styles from "@/styles/Home.module.css";
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-coverflow';
+import { EffectCoverflow, Mousewheel, Pagination, Fade, Keyboard } from 'swiper/modules'; 
+
 //FUNCTIONS
 const useMousePosition = () => {
   //Tieni traccia del movimento del mouse
@@ -28,6 +34,15 @@ const useMousePosition = () => {
 
 export default function Home() {
   const { state, dispatch } = useContext(MainContext);
+  
+  // State/function relative allo swiper
+  const [pageValue, setPageValue] = useState()
+  const [distanceValue, setDistanceValue] = useState(0)
+
+  const distanceCounter = (reference) => {
+      setPageValue(reference.realIndex)
+      setDistanceValue(distanceValue + 1)
+  }
 
   const { x, y } = useMousePosition();
 
@@ -50,6 +65,43 @@ export default function Home() {
           className={styles.SpacecraftCursor}
           src={`/spacecraft/${state.spacecraft}.png`}
           style={{ position: "absolute", left: x, top: y }}></img>
+        
+        <Swiper
+            direction={'vertical'}
+            className={`mySwiper ${styles.swiperv}`}
+            spaceBetween={1000}
+            parallax={true}
+            slidesPerView={'1'}
+            mousewheel={{sensitivity: 0, releaseOnEdges: "true"}}
+            speed={1500}
+            effect='coverflow'
+            coverflowEffect={{ //Modifica animazione in ingresso
+              rotate: 0,
+              stretch: 0,
+              scale: 200,
+              depth: 500,
+              modifier: 0.8,
+              slideShadows: false,
+            }}
+            keyboard={{enabled: true}}
+            onSlideChange={(swiper) => distanceCounter(swiper)}
+            modules={[EffectCoverflow, Mousewheel, Keyboard]}
+            initialSlide={3}      
+        >
+            <SwiperSlide className={styles.singleSlide}>
+              {/* <div position="relative">
+                  <div position="absolute/fixed">3D Model</div>
+              </div> */}
+            </SwiperSlide>
+            <SwiperSlide className={styles.singleSlide}></SwiperSlide>
+            <SwiperSlide className={styles.singleSlide}></SwiperSlide>
+            <SwiperSlide className={styles.singleSlide}></SwiperSlide>
+            <SwiperSlide className={styles.singleSlide}></SwiperSlide>
+            <SwiperSlide className={styles.singleSlide}></SwiperSlide>
+            <SwiperSlide className={styles.singleSlide}></SwiperSlide>
+            <SwiperSlide className={styles.singleSlide}></SwiperSlide>
+            <SwiperSlide className={styles.singleSlide}></SwiperSlide>
+        </Swiper>
       </main>
     </>
   );
