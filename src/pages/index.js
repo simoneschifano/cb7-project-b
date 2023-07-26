8ba381e9b47871eb34f6ede2f7c2be86d5c2ae8c
Claudio Ventimiglia <claudioxxmilia@yahoo.it>
2023-07-26 10:54:29 +0200

    feat: added login/preview page, global state , cursor

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,35 @@
 import Head from "next/head";
-
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import styles from "@/styles/Home.module.css";
+import AvatarSvg from "@/components/AvatarSvg";
+import { MainContext } from "@/state";
+import { useContext } from "react";
 
+const useMousePosition = () => {
+  //Tieni traccia del movimento del mouse
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
+  const updateMousePosition = (ev) => {
+    setMousePosition({ x: ev.clientX, y: ev.clientY });
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", updateMousePosition);
+
+    return () => window.removeEventListener("mousemove", updateMousePosition);
+  }, []);
+
+  return mousePosition;
+};
 
 export default function Home() {
+  
 
+
+  const { state, dispatch } = useContext(MainContext);
+
+  const { x, y } = useMousePosition();
 
   return (
     <>
@@ -19,26 +43,43 @@ export default function Home() {
         />
       </Head>
       <main className={styles.main}>
-        Ciao Sono la home
+        <h2>Welcome, {state.username}!</h2> 
+        <div className={styles.NavbarDetails}>
+          <div className={styles.AvatarHome}>
+            <AvatarSvg
+              skinColor={state.skinColor}
+              suitColor={state.suitColor}
+            />
+        
+          </div>
+
+          <img
+            className={styles.SpacecraftHome}
+            src={`/spacecraft/${state.spacecraft}.png`}
+          ></img>
+        </div>
+        <img
+          className={styles.SpacecraftCursor}
+          src={`/spacecraft/${state.spacecraft}.png`}
+          style={{ position: "absolute", left: x, top: y }}
+        ></img>
       </main>
     </>
   );
 }
 
-
-
 //Funzione per renderizzare sempre la rotta
 export async function getServerSideProps({ req, res }) {
   const user = req.user || req.cookies.user;
 
-  if (!user) {
+  if (!user ) {
     res.writeHead(302, { Location: "/login" });
+
+
     res.end();
   }
 
   return {
-    props: {
-      
-    },
+    props: {},
   };
 }
