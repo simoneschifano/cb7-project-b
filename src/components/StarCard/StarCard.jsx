6cc3bf433d425f62a06addf6c33475b68f1ec54d
Skinny <skinnyphoenix@icloud.com>
2023-07-29 18:42:25 +0200

    fix: entire wiki style

--- src/components/StarCard/StarCard.jsx
+++ src/components/StarCard/StarCard.jsx
@@ -1,23 +1,23 @@
-import styles from './StarCard.module.scss'
+import styles from "../PlanetCard/PlanetCard.module.scss";
 
 const StarCard = ({ data }) => {
-    return (
-        <>
-            <div className={styles.card}>
+  return (
+    <>
+      <div className={styles.Card__Wrapper}>
+        <div className={styles.Card__Main}>
+          {data.name && <h3> Name:{data.name} </h3>}
 
-                {data.name && <h3> name:{data.name} </h3>} 
+          {data.constellation && <h3> Constellation:{data.constellation} </h3>}
 
-                {data.constellation && <h3> constellation:{data.constellation} </h3>} 
+          {data.right_ascension && <h3> Right Ascension:{data.right_ascension} </h3>}
 
-                {data.right_ascension && <h3> right_ascension:{data.right_ascension} </h3>} 
+          {data.spectral_class && <h3> Spectral Class:{data.spectral_class} </h3>}
 
-                {data.spectral_class && <h3> spectral class:{data.spectral_class} </h3>}  
-
-                {data.distance_light_year && <h3> distance light year:{data.distance_light_year} </h3>} 
-                
-            </div>
-        </>
-    )
-}
+          {data.distance_light_year && <h3> Distance Light Year:{data.distance_light_year} </h3>}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default StarCard;
