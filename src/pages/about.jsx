76a927079a895612d5a5359593e765f55f835264
Skinny <skinnyphoenix@icloud.com>
2023-08-02 16:20:10 +0200

    fix: small fixes

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,82 +10,84 @@ export default function About() {
           <h1>About Us</h1>
           <h3>Developed with ❤️ by:</h3>
         </div>
-        <div className={styles.Wrapper__Devs}>
-          <div className={styles.Container__Dev__1}>
-            <Image src={"/imgs/skinny.jpeg"} width={250} height={250} alt="Giovanni Geraci" />
-            <h3>Giovanni Geraci</h3>
-            <p>Front-End Developer</p>
-            <div className={styles.Wrapper__Buttons}>
-              <button>
-                <a href="https://github.com/skinnyphoenix">Github</a>
-              </button>
-              <button>
-                <a href="https://www.linkedin.com/in/giovannigeraci00/"> LinkedIn</a>
-              </button>
+        <main>
+          <div className={styles.Wrapper__Devs}>
+            <div className={styles.Container__Dev__1}>
+              <Image src={"/imgs/skinny.jpeg"} width={250} height={250} alt="Giovanni Geraci" />
+              <h3>Giovanni Geraci</h3>
+              <p>Front-End Developer</p>
+              <div className={styles.Wrapper__Buttons}>
+                <button>
+                  <a href="https://github.com/skinnyphoenix">Github</a>
+                </button>
+                <button>
+                  <a href="https://www.linkedin.com/in/giovannigeraci00/"> LinkedIn</a>
+                </button>
+              </div>
             </div>
-          </div>
-          <div className={styles.Container__Dev__2}>
-            <Image src={"/imgs/salvo.jpeg"} width={250} height={250} alt="Salvo Falcone" />
-            <h3>Salvo Falcone</h3>
-            <p>Front-End Developer</p>
-            <div className={styles.Wrapper__Buttons}>
-              <button>
-                <a href="https://github.com/salvofalcone">Github</a>{" "}
-              </button>
-              <button>
-                <a href="https://www.linkedin.com/in/salvofalcone/"> LinkedIn</a>
-              </button>
+            <div className={styles.Container__Dev__2}>
+              <Image src={"/imgs/salvo.jpeg"} width={250} height={250} alt="Salvo Falcone" />
+              <h3>Salvo Falcone</h3>
+              <p>Front-End Developer</p>
+              <div className={styles.Wrapper__Buttons}>
+                <button>
+                  <a href="https://github.com/salvofalcone">Github</a>{" "}
+                </button>
+                <button>
+                  <a href="https://www.linkedin.com/in/salvofalcone/"> LinkedIn</a>
+                </button>
+              </div>
             </div>
-          </div>
-          <div className={styles.Container__Dev__3}>
-            <Image src={"/imgs/claudio.jpeg"} width={250} height={250} alt="Claudio Ventimiglia" />
-            <h3>Claudio Ventimiglia</h3>
-            <p>Front-End Developer</p>
-            <div className={styles.Wrapper__Buttons}>
-              <button>
-                <a href="https://github.com/Miles0908">Github</a>{" "}
-              </button>
-              <button>
-                <a href="https://www.linkedin.com/in/claudio-ventimiglia-402305273/"> LinkedIn</a>
-              </button>
+            <div className={styles.Container__Dev__3}>
+              <Image src={"/imgs/claudio.jpeg"} width={250} height={250} alt="Claudio Ventimiglia" />
+              <h3>Claudio Ventimiglia</h3>
+              <p>Front-End Developer</p>
+              <div className={styles.Wrapper__Buttons}>
+                <button>
+                  <a href="https://github.com/Miles0908">Github</a>{" "}
+                </button>
+                <button>
+                  <a href="https://www.linkedin.com/in/claudio-ventimiglia-402305273/"> LinkedIn</a>
+                </button>
+              </div>
             </div>
-          </div>
-          <div className={styles.Container__Dev__4}>
-            <Image src={"/imgs/gabri.jpeg"} width={250} height={250} alt="Gabriele Salemi" />
-            <h3>Gabriele Salemi</h3>
-            <p>Front-End Developer</p>
-            <div className={styles.Wrapper__Buttons}>
-              <button>
-                <a href="https://github.com/GabSalemi">Github</a>
-              </button>
-              <button>
-                <a href="https://www.linkedin.com/in/gabriele-salemi-450869207/"> LinkedIn</a>
-              </button>
+            <div className={styles.Container__Dev__4}>
+              <Image src={"/imgs/gabri.jpeg"} width={250} height={250} alt="Gabriele Salemi" />
+              <h3>Gabriele Salemi</h3>
+              <p>Front-End Developer</p>
+              <div className={styles.Wrapper__Buttons}>
+                <button>
+                  <a href="https://github.com/GabSalemi">Github</a>
+                </button>
+                <button>
+                  <a href="https://www.linkedin.com/in/gabriele-salemi-450869207/"> LinkedIn</a>
+                </button>
+              </div>
             </div>
-          </div>
-          <div className={styles.Container__Dev__5}>
-            <Image src={"/imgs/abdel.jpeg"} width={250} height={250} alt="Abdel Ibnorida" />
-            <h3>Abdel Ibnorida</h3>
-            <p>Front-End Developer</p>
-            <div className={styles.Wrapper__Buttons}>
-              <button>
-                <a href="https://github.com/abdel-ibnorida">Github</a>
-              </button>
-              <button>
-                <a href="https://www.linkedin.com/in/abdel-ibnorida/"> LinkedIn</a>
-              </button>
+            <div className={styles.Container__Dev__5}>
+              <Image src={"/imgs/abdel.jpeg"} width={250} height={250} alt="Abdel Ibnorida" />
+              <h3>Abdel Ibnorida</h3>
+              <p>Front-End Developer</p>
+              <div className={styles.Wrapper__Buttons}>
+                <button>
+                  <a href="https://github.com/abdel-ibnorida">Github</a>
+                </button>
+                <button>
+                  <a href="https://www.linkedin.com/in/abdel-ibnorida/"> LinkedIn</a>
+                </button>
+              </div>
             </div>
-          </div>
-          <h3 className={styles.paragraph}>
-            Questo progetto punta ad avvicinare gli utenti allo spazio, argomento sempre più centrale nella società odierna. Grazie all’utilizzo di
-            modelli 3D l’utente avrà la possibilità di creare il proprio avatar e scegliere un’astronave per affrontare un viaggio alla scoperta dei
-            pianeti del sistema solare. Ogni pianeta avrà una card con le informazioni relative, così da unire la parte ludica del viaggio con quella
-            informativa. La sezione Wiki sarà dedicata alla ricerca di corpi celesti anche esterni al Sistema Solare…l’unico limite è quello posto da
-            noi stessi! 🚀
-          </h3>
+            <h3 className={styles.paragraph}>
+              Questo progetto punta ad avvicinare gli utenti allo spazio, argomento sempre più centrale nella società odierna. Grazie all’utilizzo di
+              modelli 3D l’utente avrà la possibilità di creare il proprio avatar e scegliere un’astronave per affrontare un viaggio alla scoperta dei
+              pianeti del sistema solare. Ogni pianeta avrà una card con le informazioni relative, così da unire la parte ludica del viaggio con
+              quella informativa. La sezione Wiki sarà dedicata alla ricerca di corpi celesti anche esterni al Sistema Solare…l’unico limite è quello
+              posto da noi stessi! 🚀
+            </h3>
 
-          <p className={styles.paragraph}>Designed and Coded with ❤️. Final Project x Edgemony 💎</p>
-        </div>
+            <p className={styles.paragraph}>Designed and Coded with ❤️. Final Project x Edgemony 💎</p>
+          </div>
+        </main>
       </div>
     </>
   );
